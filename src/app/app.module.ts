ffe547f3be8764d7c636148007df98f9fe5025fd
Daniel Shell <dshell321@gmail.com>
2019-08-01 20:30:58 +1000

    Refactor routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(APP_ROUTES, { enableTracing: false})
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
